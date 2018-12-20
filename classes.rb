ba730c1d0ca948c8b92223db1c0983d92f2e4173
costagavras <cgavras@yahoo.ca>
2018-12-19 23:31:39 -0500

    modules

--- classes.rb
+++ classes.rb
@@ -1,3 +1,4 @@
+#nested classes
 class Drawing
   def self.give_me_a_circle
     Circle.new
@@ -17,5 +18,25 @@ a = Drawing.give_me_a_circle
 puts a.what_am_i
 a = Drawing::Circle.new
 puts a.what_am_i
-a = Circle.new
-puts a.what_am_i
+# a = Circle.new
+# puts a.what_am_i
+
+#Constants are defined within the scope of the current class and
+#made available to all child classes, unless they're overriden.
+
+#Modules
+
+module NumberStuff
+  def self.random
+    rand(1000)
+  end
+end
+
+module LetterStuff
+  def self.random
+    (rand(26) + 65).chr
+  end
+end
+
+puts NumberStuff.random
+puts LetterStuff.random
