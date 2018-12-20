2680bb9dd37386306d1dfe2ff66bdf1ab09582d3
costagavras <cgavras@yahoo.ca>
2018-12-19 23:54:42 -0500

    loop .each

--- classes.rb
+++ classes.rb
@@ -79,3 +79,12 @@ end
 
 include AnotherModule
 do_stuff
+
+#longest word check
+my_array = %w{this is a test of the longest word check}
+longest_word = ""
+my_array.each do |word|
+  longest_word = word if longest_word.length < word.length
+end
+
+puts longest_word
