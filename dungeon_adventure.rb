e3fc9b82a7c047b245d7f2b2df02ae470a87dbf0
costagavras <cgavras@yahoo.ca>
2018-12-20 19:51:06 -0500

    with Structs

--- dungeon_adventure.rb
+++ dungeon_adventure.rb
@@ -2,27 +2,35 @@ class Dungeon
   attr_accessor :player
 
   def initialize(player)
-    @player = player
+    @player = player #@player contains Player object
     @rooms = {}
   end
 
 end
 
-class Player
-  attr_accessor :name, :location
+# class Player
+#   attr_accessor :name, :location
+#
+#   def initialize(player_name)
+#     @name = player_name
+#   end
+# end
 
-  def initialize(player_name)
-    @name = player_name
-  end
-end
+Player = Struct.new(:name, :location) #optional parametres
+#
+# class Room
+#   attr_accessor :reference, :name, :description, :connections
+#
+#   def initialize(reference, name, description, connections)
+#     @reference = reference
+#     @name = name
+#     @description = description
+#     @connections = connections
+#   end
+# end
 
-class Room
-  attr_accessor :reference, :name, :description, :connections
+Room = Struct.new(:reference, :name, :description, :connections) #optional parametres
 
-  def initialize(reference, name, description, connections)
-    @reference = reference
-    @name = name
-    @description = description
-    @connections = connections    
-  end
-end
+me = Player.new("Costa") #creates a Player object
+my_dungeon = Dungeon.new(me) #passes a player object to the newly created Dungeon object
+puts my_dungeon.player.name
