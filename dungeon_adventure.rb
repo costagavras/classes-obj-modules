71538593f3a4722b8ed65404b6c4466d4b579d8b
costagavras <cgavras@yahoo.ca>
2018-12-20 20:24:12 -0500

    working_structure

--- dungeon_adventure.rb
+++ dungeon_adventure.rb
@@ -6,31 +6,67 @@ class Dungeon
     @rooms = {}
   end
 
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
+
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
+
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms[reference]
+  end
+
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player.location).connections[direction]
+  end
+
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_direction(direction)
+    show_current_description
+  end
+
 end
 
-# class Player
-#   attr_accessor :name, :location
-#
-#   def initialize(player_name)
-#     @name = player_name
-#   end
-# end
+class Player
+  attr_accessor :name, :location
 
-Player = Struct.new(:name, :location) #optional parametres
-#
-# class Room
-#   attr_accessor :reference, :name, :description, :connections
-#
-#   def initialize(reference, name, description, connections)
-#     @reference = reference
-#     @name = name
-#     @description = description
-#     @connections = connections
-#   end
-# end
+  def initialize(name)
+    @name = name
+  end
+end
 
-Room = Struct.new(:reference, :name, :description, :connections) #optional parametres
+class Room
+  attr_accessor :reference, :name, :description, :connections
 
-me = Player.new("Costa") #creates a Player object
-my_dungeon = Dungeon.new(me) #passes a player object to the newly created Dungeon object
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+
+  def full_description
+    @name + "\n\nYou are in " + @description
+  end
+
+end
+
+# Player = Struct.new(:name, :location) #optional parametres
+# Room = Struct.new(:reference, :name, :description, :connections) #optional parametres
+
+player = Player.new("Costa") #creates a Player object
+my_dungeon = Dungeon.new(player) #passes a player object to the newly created Dungeon object
 puts my_dungeon.player.name
+puts
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave})
+my_dungeon.add_room(:smallcave, "Small Cave", "a small claustrophobic cave", {:east => :largecave})
+
+my_dungeon.start(:largecave)
