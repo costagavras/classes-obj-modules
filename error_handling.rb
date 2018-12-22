c28707a82b3f802bdd2b1691b79006ffc7cf7c68
costagavras <cgavras@yahoo.ca>
2018-12-21 21:32:34 -0500

    handling_exceptins

--- error_handling.rb
+++ error_handling.rb
@@ -1,7 +1,40 @@
-class Person
-  def initialize(name)
-    raise ArgumentError, "No name present" if name.empty?
-  end
+# class Person
+#   def initialize(name)
+#     raise ArgumentError, "No name present" if name.empty?
+#   end
+# end
+#
+# fred = Person.new("")
+
+#handling exceptions
+begin
+  puts 10 / 0
+rescue
+  puts "You caused an error!"
 end
 
-fred = Person.new("")
+#identifying exceptions
+begin
+  puts 10 / 0
+rescue => e
+  puts e.class
+end
+
+catch(:finish) do
+  1000.times do
+    x = rand(1000)
+    throw :finish if x == 123
+  end
+
+  puts "Generated 1000 random numbers without generating 123"
+end
+
+def generate_random_number_bar_123
+  x = rand(1000)
+  throw :finish if x == 123
+end
+
+catch(:finish) do
+  1000.times{generate_random_number_bar_123}
+  puts "Generated 1000 random numbers without generating 123!"
+end
