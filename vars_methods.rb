30d3a8e98205c4349733d715f750f299bb3dee6d
costagavras <cgavras@yahoo.ca>
2019-01-14 19:38:42 -0500

    metal_example

--- vars_methods.rb
+++ vars_methods.rb
@@ -9,11 +9,11 @@ class Metal
     @melting = melt
   end
 
-  def self.current_temp=(x)
+  def self.current_temp=(x) #setter
     @@current_temp = x
   end
 
-  def self.current_temp
+  def self.current_temp #getter
     @@current_temp
   end
 
@@ -23,6 +23,18 @@ class Metal
 
 end
 
-aluminum = Metal.new(13, 1236)
+aluminium = Metal.new(13, 1236)
 copper = Metal.new(29, 1982)
 gold = Metal.new(79, 1948)
+
+Metal.current_temp = 1700
+
+puts aluminium.liquid?
+puts copper.liquid?
+puts gold.liquid?
+
+Metal.current_temp = 2100
+
+puts aluminium.liquid?
+puts copper.liquid?
+puts gold.liquid?
